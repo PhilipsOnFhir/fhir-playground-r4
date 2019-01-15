5eff1eb894da4d13dc41f55e9ce5f3a27507990a
CODE1\nly98977 <bas.van.den.heuvel@philips.com>
2019-01-15 11:18:09 -0500

    Java Server Java WebSub app Angular2 rest client working

---
+++ rest/src/app/service/fhir-cast-rest.service.ts
@@ -0,0 +1,54 @@
+import {Injectable, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FhirCastRestService {
+  private topicUrl: string ;
+  private status: string;
+  private context: string;
+
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) {
+    this.status = 'UNINITIALIZED';
+    // Note: Below 'queryParams' can be replaced with 'params' depending on your requirements
+    this.activatedRoute.queryParams.subscribe(params => {
+      console.log(params);
+      this.topicUrl = params['hub'];
+      console.log(this.topicUrl);
+    });
+
+  }
+
+  updateContext(): Observable<string> {
+    return new Observable<string>(observer => {
+        this.http.get<string>(this.topicUrl).subscribe(
+          context => {
+            console.log('context retrieved');
+            console.log(context);
+            this.context = context;
+            this.status = 'UPDATED';
+            observer.next(context);
+            observer.complete();
+          }, err => {
+            console.log(err);
+            this.status = 'ERROR';
+            observer.error(err);
+          });
+      });
+  }
+
+  getStatus() {
+    return this.status;
+  }
+
+  getContext() {
+    return this.context;
+  }
+
+  getTopicUrl() {
+    return this.topicUrl;
+  }
+}
--- src/main/java/com/philips/research/philipsonfhir/fhirproxy/dstu3/applications/fhircast/server/controller/MyFhirCastController.java
+++
@@ -1,8 +0,0 @@
-package com.philips.research.philipsonfhir.fhirproxy.dstu3.applications.fhircast.server.controller;
-
-import com.philips.research.philipsonfhir.fhirproxy.dstu3.support.fhircast.controller.FhirCastController;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class MyFhirCastController extends FhirCastController {
-}
