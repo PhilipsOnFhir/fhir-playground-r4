09e133d6e14d5c4c6072b4115d4293afd275a824
bvdh <PIW5Y6dirznn8PiXVmwC>
2019-06-11 15:26:29 +0200

    OAuth server working HAPI server r4 working fhircast moved to modle

---
+++ authserver/src/main/java/org/github/philipsonfhir/fhircast/auth/AuthorizationSecurityConfig.java
@@ -0,0 +1,64 @@
+package org.github.philipsonfhir.fhircast.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+/**
+ * Created by zakaria on 16/07/17.
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationSecurityConfig extends AuthorizationServerConfigurerAdapter {
+
+//    @Autowired
+//    private AuthenticationManager authenticationManager;
+//
+//
+//    @Override
+//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//        endpoints.authenticationManager(this.authenticationManager);
+//    }
+
+
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("my-trusted-client")
+                    .authorizedGrantTypes("password",
+                            "refresh_token", "implicit", "client_credentials", "authorization_code")
+                    .authorities("CLIENT")
+                    .scopes("read", "write", "trust")
+                    .accessTokenValiditySeconds(3600)
+                    .redirectUris("http://localhost:8081/test.html")
+//                    .resourceIds("resource")
+                    .secret("mysecret")
+                .and()
+                .withClient("fhircasthub")
+                        .authorizedGrantTypes("password",
+                                "refresh_token", "implicit", "client_credentials", "authorization_code")
+                        .authorities("CLIENT")
+                        .scopes("read", "write", "trust")
+                        .accessTokenValiditySeconds(3600)
+                        .redirectUris("http://localhost:8080/test")
+//                        .resourceIds("resource")
+                        .secret("fhircast-secret");
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer)
+            throws Exception {
+        oauthServer
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("permitAll()");
+    }
+}
---
+++ authserver/src/main/java/org/github/philipsonfhir/fhircast/auth/WebSecurityConfig.java
@@ -0,0 +1,86 @@
+package org.github.philipsonfhir.fhircast.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Properties;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(encoder.encode("admin")).authorities("ROLE_USER");
+    }
+
+//    @Bean
+//    @Override
+//    public UserDetailsService userDetailsService() {
+//        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+//        inMemoryUserDetailsManager
+//                .
+//                .createUser( User.withUsername("admin").password("admin").roles("ADMIN").build());
+//
+//        return inMemoryUserDetailsManager;
+//        return new InMemoryUserDetailsManager(
+//                User.withDefaultPasswordEncoder()
+//                        .username("enduser")
+//                        .password("password")
+//                        .roles("USER")
+//                        .build());
+//    }
+
+//    @Bean
+//    public AuthenticationSuccessHandler myAuthenticationSuccessHandler(){
+//        return new MySimpleUrlAuthenticationSuccessHandler();
+//    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+            .and()
+//                .formLogin().defaultSuccessUrl("/test.html")
+//                .formLogin().successHandler(myAuthenticationSuccessHandler())
+                .formLogin()
+            .and()
+                .logout().permitAll()
+            .and()
+                .csrf().disable();
+    }
+}
